58a24cef00da55edc6fd8f83f621d4125bc4504f
Shanks <shankarkannan13@gmail.com>
2023-12-27 20:39:23 +0530

    CSS Tuts completed 27-12-23

---
+++ Video 54 -JS Intro/new.js
@@ -0,0 +1,4 @@
+console.log("hello node.js");
+console.log("I want big coder money ty");
+
+// remember that features like alert and prompt are browser stuff so it wont work here
---
+++ Video 54 -JS Intro/script.js
@@ -0,0 +1,12 @@
+alert("Hello world from a script file");
+console.log("code running");
+console.error("code is f**ked");
+console.warn("Code may F**k smth");
+
+let a=prompt("enter your number");
+if(a==7){
+    console.log("THALA FOR A REASON");
+}
+console.log("Hello",a);
+document.title="YOOO JS INTRO HERE WE GO";
+document.body.style.backgroundColor = "red";
